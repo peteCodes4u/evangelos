a9b9635ccc36eaf63d70d34c5ee4faa463ae40b1
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-19 13:54:01 -0400

    Completed the index files for api routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = reuire('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    return res.status(500).send('Something went wrong! we apologize for the inconvience');
+});
+
+module.exports = router;
